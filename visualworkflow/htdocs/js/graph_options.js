73b609eb7531a9eb78db6500904ebe995cfb9e45
Mat Booth <mbooth@apache.org>
2009-05-17 01:42:50 +0000

    Only reload the graph image once on page load to reduce the number of broken pipes occurring.

--- visualworkflow/htdocs/js/graph_options.js
+++ visualworkflow/htdocs/js/graph_options.js
@@ -2,20 +2,28 @@
  * This script reloads the graph image when one of its display options is changed.
  */
 
-jQuery(document).ready(function($) {
-  
-  // save the base url of the image for use when updating the source url inside the change event
-  $("#graph").data("src", $("#graph").attr("src"));
-  $(".graph_option").change(function() {
+(function($){
+  window.reloadGraph = function(elm) {
     var params = new Array();
     
     // add time as a parameter so the browser doesn't cache out of date images
-    params.push("time=" + new Date().getUTCMilliseconds());
+    params.push("time=" + new Date().getTime());
     
     if ($("#ops").attr("checked"))
       params.push($("#ops").attr("value") + "=1");
     if ($("#perms").attr("checked"))
       params.push($("#perms").attr("value") + "=1");
     $("#graph").attr("src", $("#graph").data("src") + "?" + params.join("&amp;"));
-  }).change();
+  }
+})(jQuery);
+
+jQuery(document).ready(function($) {
+  
+  // save the base url of the image for use when updating the source url inside the change event
+  $("#graph").data("src", $("#graph").attr("src"));
+  
+  // add the change events and call the reload function once on page load, to make
+  // sure the options are in sync with the image
+  $(".graph_option").change(function() { reloadGraph(this) });
+  window.reloadGraph();
 });
