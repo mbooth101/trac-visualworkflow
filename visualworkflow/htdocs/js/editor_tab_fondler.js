41e3ab2c94aac7aa1afce8da609d9a53378e91c1
Mat Booth <mbooth@apache.org>
2009-05-17 23:19:10 +0000

    Typo in last commit.

--- visualworkflow/htdocs/js/editor_tab_fondler.js
+++ visualworkflow/htdocs/js/editor_tab_fondler.js
@@ -14,7 +14,7 @@
     
     // hide all active panels and unhide the panel that corresponds with this tab
     $("#editor_tabcontent div.active").removeClass("active").addClass("inactive");
-    $("#content_" + $(elm).parent().attr("id").split("_")[1]).removeClass("active").addClass("active");
+    $("#content_" + $(elm).parent().attr("id").split("_")[1]).removeClass("inactive").addClass("active");
     
     // add links to the inactive tabs and remove the link from the newly active tab
     $("#editor_tabs li[class!=active]").each(function() { addLink(this) });
